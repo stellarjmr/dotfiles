751513aab17b47a48f5f2b09d261c3806eb8085e
stellarjmr <opener48_potion@icloud.com>
2025-07-31 19:53:38 +0100

    updates

--- nvim/lua/config/conform.lua
+++ nvim/lua/config/conform.lua
@@ -4,9 +4,9 @@ return {
   opts = {
     formatters_by_ft = {
       lua = { "stylua" },
-      -- python = { "black" },
+      python = { "black" },
       sh = { "shfmt" },
-      -- tex = { "tex-fmt" },
+      tex = { "tex-fmt" },
     },
     format_on_save = { timeout_ms = 1000, lsp_format = "fallback" },
   },
