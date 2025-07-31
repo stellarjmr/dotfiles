6d9a93b4fa42876912832184d2480ba22a59dce6
stellarjmr <opener48_potion@icloud.com>
2025-07-31 22:49:21 +0100

    update conform plugin

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -4,6 +4,10 @@ return {
   opts = function(_, opts)
     opts.formatters_by_ft = vim.tbl_deep_extend("force", opts.formatters_by_ft or {}, {
       ["_"] = { "trim_whitespace" },
+      lua = { "stylua" },
+      python = { "black" },
+      sh = { "shfmt" },
+      tex = { "tex-fmt" },
     })
   end,
 }
