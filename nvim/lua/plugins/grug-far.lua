3491125e7c9eb6086d885bb106a31645703ea64b
stellarjmr <opener48_potion@icloud.com>
2025-08-21 13:26:01 +0100

    update grug-far

---
+++ nvim/lua/plugins/grug-far.lua
@@ -0,0 +1,24 @@
+return {
+  "MagicDuck/grug-far.nvim",
+  opts = { headerMaxWidth = 80 },
+  cmd = "GrugFar",
+  keys = {
+    {
+      "<leader>sr",
+      function()
+        local grug = require("grug-far")
+        local ext = vim.bo.buftype == "" and vim.fn.expand("%:e")
+        grug.open({
+          transient = true,
+          prefills = {
+            -- Limiting search/replace to current file
+            paths = vim.fn.expand("%"),
+            filesFilter = ext and ext ~= "" and "*." .. ext or nil,
+          },
+        })
+      end,
+      mode = { "n", "v" },
+      desc = "Search and Replace",
+    },
+  },
+}
