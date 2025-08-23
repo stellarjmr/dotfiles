b16e994ee5db1a570c0bd4797f5b94271f2761af
stellarjmr <opener48_potion@icloud.com>
2025-08-23 20:37:22 +0100

    updates

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -27,8 +27,28 @@ return {
     picker = {
       sources = {
         explorer = {
+          --- float explorer
+          -- layout = {
+          --   { preview = true },
+          --   layout = {
+          --     box = "horizontal",
+          --     width = 0.8,
+          --     height = 0.8,
+          --     {
+          --       box = "vertical",
+          --       border = "rounded",
+          --       title = "{source} {live} {flags}",
+          --       title_pos = "center",
+          --       { win = "input", height = 1, border = "bottom" },
+          --       { win = "list", border = "none" },
+          --     },
+          --     { win = "preview", border = "rounded", width = 0.7, title = "{preview}" },
+          --   },
+          -- },
+          --- normal explorer
           layout = {
             layout = {
+              position = "right",
               width = 25,
             },
           },
