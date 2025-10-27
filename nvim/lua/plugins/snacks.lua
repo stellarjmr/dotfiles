6fff16a24f27e10c97fe488a135b09fd64fa9ec2
stellarjmr <opener48_potion@icloud.com>
2025-10-27 14:47:21 +0000

    add keymap to projects and notes

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -1,5 +1,37 @@
 return {
   "folke/snacks.nvim",
+  keys = {
+    {
+      "<leader>p",
+      function()
+        -- Define your project folders here (same as in opts)
+        local project_folders = {
+          "~/path/to/project1",
+          "~/path/to/project2",
+          "~/path/to/project3",
+          "~/path/to/project4",
+          "~/path/to/project5",
+        }
+        vim.ui.select(project_folders, {
+          prompt = "Select Project:",
+        }, function(choice)
+          if choice then
+            local expanded_path = vim.fn.expand(choice)
+            vim.cmd("cd " .. vim.fn.fnameescape(expanded_path))
+            vim.cmd(":lua Snacks.dashboard.pick('files', { cwd = expanded_path })")
+          end
+        end)
+      end,
+      desc = "Open Project Manager",
+    },
+    {
+      "<leader>o",
+      function()
+        require("snacks").picker.pick("files", { cwd = "~/Documents/Notes" })
+      end,
+      desc = "Open Notes",
+    },
+  },
   ---@type snacks.Config
   opts = function()
     -- Define your project folders here
