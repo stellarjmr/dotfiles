ed49408ff9024279e7357e56f4577ce49ef1ce3b
stellarjmr <opener48_potion@icloud.com>
2025-11-01 16:06:59 +0000

    add sidekick

---
+++ nvim/lua/plugins/lualine.lua
@@ -0,0 +1,21 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  opts = function(_, opts)
+    opts.sections = opts.sections or {}
+    opts.sections.lualine_c = opts.sections.lualine_c or {}
+
+    -- CLI session status
+    table.insert(opts.sections.lualine_x, 2, {
+      function()
+        local status = require("sidekick.status").cli()
+        return " " .. (#status > 1 and #status or "")
+      end,
+      cond = function()
+        return #require("sidekick.status").cli() > 0
+      end,
+      color = function()
+        return "Special"
+      end,
+    })
+  end,
+}
