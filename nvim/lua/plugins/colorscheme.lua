db169d0d459526835de92d9f77c9c0e6fb08b458
stellarjmr <opener48_potion@icloud.com>
2025-11-03 11:19:06 +0000

    updates

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -11,8 +11,8 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-      vim.opt.background = "light"
-      vim.g.everforest_background = "soft"
+      vim.opt.background = "dark"
+      vim.g.everforest_background = "hard"
       vim.g.everforest_transparent_background = 2
       vim.g.everforest_current_word = "bold"
       vim.g.everforest_enable_italic = 1
