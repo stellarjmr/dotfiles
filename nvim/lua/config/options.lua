b6512b9703650bb1a2b4412190416b7d307f9dfc
stellarjmr <opener48_potion@icloud.com>
2025-11-06 15:27:16 +0000

    updates

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,3 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.diagnostic.enable(false)
+
+-- Disable auto-formatting on save (LazyVim's format system)
+vim.g.autoformat = false
