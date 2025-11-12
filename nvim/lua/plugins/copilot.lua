44b98facfd6ca1603bcb45fac99d2fd2e74b5ee5
stellarjmr <opener48_potion@icloud.com>
2025-11-12 14:38:14 +0000

    use copilot for specific file type

---
+++ nvim/lua/plugins/copilot.lua
@@ -0,0 +1,9 @@
+return {
+  "zbirenbaum/copilot.lua",
+  opts = {
+    filetypes = {
+      ["*"] = false, -- disable for all other filetypes and ignore default `filetypes`
+      python = true,
+    },
+  },
+}
