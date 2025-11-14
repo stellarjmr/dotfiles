661f84dced95bf2fccbdee863fc79248ebd594a0
stellarjmr <opener48_potion@icloud.com>
2025-11-14 17:22:45 +0000

    update

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,6 +1,7 @@
 return {
   "zbirenbaum/copilot.lua",
   opts = {
+    disable_limit_reached_message = true,
     filetypes = {
       ["*"] = false, -- disable for all other filetypes and ignore default `filetypes`
       python = true,
