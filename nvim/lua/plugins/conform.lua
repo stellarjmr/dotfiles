700fed119521970ff6a7688732caa457ca3f2cc7
stellarjmr <opener48_potion@icloud.com>
2025-11-23 20:08:46 +0000

    updates

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -5,14 +5,7 @@ return {
     opts.formatters_by_ft = vim.tbl_deep_extend("force", opts.formatters_by_ft or {}, {
       ["_"] = { "trim_whitespace" },
       lua = { "stylua" },
-      python = {
-        --   -- To fix auto-fixable lint errors.
-        --   "ruff_fix",
-        --   -- To run the Ruff formatter.
-        "ruff_format",
-        --   -- To organize the imports.
-        --   "ruff_organize_imports",
-      },
+      python = { "ruff_format" },
       sh = { "shfmt" },
       tex = { "tex-fmt" },
     })
