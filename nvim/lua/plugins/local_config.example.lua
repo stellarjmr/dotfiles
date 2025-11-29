89582600faf60448933dec5493db16285c235d7c
stellarjmr <opener48_potion@icloud.com>
2025-11-29 14:37:36 +0000

    refactor: move project folders to separate config file
    Move project folder paths from snacks.lua to local_config.lua to keep
    personal paths out of version control. Add local_config.example.lua as
    a template for new setups.

---
+++ nvim/lua/plugins/local_config.example.lua
@@ -0,0 +1,11 @@
+-- Example local configuration file
+-- Copy this file to local_config.lua and edit with your personal paths
+
+return {
+  project_folders = {
+    "~/path/to/project1",
+    "~/path/to/project2",
+    "~/path/to/project3",
+    -- Add more project folders here
+  },
+}
