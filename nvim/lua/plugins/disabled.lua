0b52c554a9ac9a1060348c12399c4f83f4c4e76f
stellarjmr <opener48_potion@icloud.com>
2025-12-04 17:49:45 +0000

    updates: kitty inline python plot support

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -1,4 +1,6 @@
 return {
   { "catppuccin/nvim", enabled = false },
   { "folke/tokyonight.nvim", enabled = false },
+  -- { "jpalardy/vim-slime", enabled = false },
+  { "hkupty/iron.nvim", enabled = false },
 }
--- nvim/lua/plugins/vim-slime.lua
+++ nvim/lua/plugins/vim-slime.lua
@@ -1,6 +1,6 @@
 return {
   "jpalardy/vim-slime",
-  enabled = false,
+  ft = { "python" },
   keys = {
     {
       "<leader>sc",
